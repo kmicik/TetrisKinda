fa19cbcd2fc213184b71b7feb70352f50bcab1fd
kmicik <kmicik>
2018-04-30 02:28:01 -0500

    Strings added to About

--- app/src/main/java/com/mangrummicik/tetriskinda/MainActivity.java
+++ app/src/main/java/com/mangrummicik/tetriskinda/MainActivity.java
@@ -23,5 +23,12 @@ public class MainActivity extends Activity {
         });
 
         b = findViewById(R.id.about_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, About.class));
+
+            }
+        });
     }
 }
